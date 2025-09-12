4996cea52fecd6ec1830ff9b587fe4077c4ccc42
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-09-13 03:09:43 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,9 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
         >
-        {children}
-        </ThemeProvider>                                          
+          {children}
+          <Analytics />
+        </ThemeProvider>
       </body>
     </html>
   );
